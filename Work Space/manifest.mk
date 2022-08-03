323b75413a001d3446ec8614952b85e9f4cad0d2
jhorowitz99 <contact@josephbhorowitz.com>
2022-08-03 13:47:22 -0400

    Inital Setup

---
+++ Work Space/manifest.mk
@@ -0,0 +1,17 @@
+#Main Page
+##Header
+* Profile Pic
+* Hi, My name is Joseph Horowitz
+* Job title
+* linkedin, hover email, resume
+* down arrow
+##Content
+* project title
+* img
+* short description
+* tags 
+* view project
+##Projects
+* 3M
+* Weekly UX AR Challenges
+* 
